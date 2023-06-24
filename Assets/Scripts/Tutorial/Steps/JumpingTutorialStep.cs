cd5627c964fc75e5f1e5b5f4951a5a4c944fb05b
Arnilsen Arthur <arnilsenarthur@gmail.com>
2023-06-24 18:59:01 -0300

    New Steps

---
+++ Assets/Scripts/Tutorial/Steps/JumpingTutorialStep.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+namespace Refactor.Tutorial.Steps
+{
+    public class JumpingTutorialStep : DefaultTutorialStep
+    {
+        public GameObject button;
+        public GameObject[] platforms;
+        
+        public override void OnBegin()
+        {
+            base.OnBegin();
+            
+            input.DisableAllInput();
+            input.canMoveCamera = true;
+            input.canMove = true;
+            input.canRun = true;
+            input.canInteract = true;
+            input.canJump = true;
+            
+            controller.ShowBindingDisplay("jump");
+            button.SetActive(true);
+
+            foreach (var plat in platforms)
+                plat.SetActive(true);
+        }
+        
+        public override void OnEnd()
+        {
+            base.OnEnd();
+            controller.ShowBindingDisplay("");
+            input.DisableAllInput();
+            
+            foreach (var plat in platforms)
+                plat.SetActive(false);
+            
+            button.SetActive(false);
+        }
+    }
+}
