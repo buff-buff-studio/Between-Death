d4eeb6d2f042e9bdafc7d9b22745088ea8f07d02
Arnilsen Arthur <arnilsenarthur@gmail.com>
2023-06-23 23:57:56 -0300

    Changes for Diego Assignment

--- Assets/Scripts/Interface/IngameCanvas.cs
+++ Assets/Scripts/Interface/IngameCanvas.cs
@@ -1,7 +1,12 @@
+using System;
 using System.Collections;
 using DG.Tweening;
+using Refactor.Entities;
+using Refactor.Entities.Modules;
 using Refactor.Interface.Windows;
+using Refactor.Misc;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Refactor.Interface
 {
@@ -9,7 +14,31 @@ namespace Refactor.Interface
     {
         public Window quitDialogWindow;
         public IngameGameInput ingameGameInput;
-        
+        public Image[] bloodStains;
+        public Entity player;
+        private float lastHealth = 0;
+        public RectTransform healthBarFill;
+
+        private void OnEnable()
+        {
+            var module = player.GetModule<HealthEntityModule>();
+            var hlt = (IHealth)module;
+            lastHealth = hlt.health;
+            module.onHealthChange.AddListener((h) =>
+            {
+                Debug.Log($"Changed: {h} {lastHealth}");
+                if (h < lastHealth)
+                {
+                    ShowDamageEffect();
+                }
+
+                lastHealth = h;
+                UpdateHealthBar(h);
+            });
+
+            UpdateHealthBar(hlt.maxHealth);
+        }
+
         public override void CallAction(InterfaceAction action)
         {
             if (action is InterfaceAction.Start && ingameGameInput.canInput)
@@ -44,6 +73,21 @@ namespace Refactor.Interface
         {
             yield return new WaitForSeconds(0.5f);
             ingameGameInput.canInput = true;
+        }
+
+        public void UpdateHealthBar(float health)
+        {
+            var module = (IHealth) player.GetModule<HealthEntityModule>();
+            healthBarFill.DOSizeDelta(new Vector2(260 * health / module.maxHealth, 20), 0.5f);
+        }
+        
+        public void ShowDamageEffect()
+        {
+            foreach (var image in bloodStains)
+            {
+                image.DOColor(Color.white, 0.5f);
+                image.DOColor(Color.clear, 0.5f).SetDelay(0.55f);
+            }
         }
     }
 }
