2ff16ada181b6b60b5b105b544c7c03c70ce62ec
Sérgio Macellani <sergiododec@gmail.com>
2023-09-04 20:59:32 -0300

    Fix Skill Data

--- Assets/Scripts/Upgrades/Skills/SkillData.cs
+++ Assets/Scripts/Upgrades/Skills/SkillData.cs
@@ -1,20 +1,34 @@
 using Refactor.Data;
+using Refactor.Entities;
 using UnityEngine;
 using UnityEngine.Video;
 
 [CreateAssetMenu(fileName = "Skill", menuName = "RPG/Skill")]
 public class SkillData : GenericItemData
 {
-    public Animation animation;
     public VideoClip preview;
     
     [Space]
     [Header("Values")]
     public Element element;
-    public int damage;
-    public int knockback;
-    [Range(0,10f)] [Tooltip("Duration in seconds")]
-    public float duration;
     [Range(0,60f)] [Tooltip("Cooldown in seconds")]
     public float cooldown;
+    
+    [Header("Attack")]
+    public string clipName;
+    public float damage = 1f;
+    
+    [Header("Timings")]
+    public float transitionTime = 0.1f;
+    public float damageTime = 0.5f;
+    
+    public Attack GetAttack()
+    {
+        var attack = CreateInstance<Attack>();
+        attack.clipName = clipName;
+        attack.damage = damage;
+        attack.transitionTime = transitionTime;
+        attack.damageTime = damageTime;
+        return attack;
+    }
 }
--- Assets/Scripts/Upgrades/Skills/SkillList.cs
+++ Assets/Scripts/Upgrades/Skills/SkillList.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Refactor.Data;
+using Refactor.Entities;
 using UnityEngine;
 using UnityEngine.Video;
 
@@ -17,15 +18,13 @@ public class SkillList : ScriptableObject
     
     //Visuals
     public Sprite GetIcon(int i) => skills[i].icon;
-    public Animation GetAnimation(int i) => skills[i].animation;
     public VideoClip GetPreview(int i) => skills[i].preview;
     
     //Values
     public Element GetElement(int i) => skills[i].element;
-    public int GetDamage(int i) => skills[i].damage;
-    public int GetKnockback(int i) => skills[i].knockback;
-    public float GetDuration(int i) => skills[i].duration;
     public float GetCooldown(int i) => skills[i].cooldown;
+    public float GetDamage(int i) => skills[i].damage;
+    public Attack GetAttack(int i) => skills[i].GetAttack();
     
     public bool IsElement(int i, Element element) => skills[i].element == element;
 }
